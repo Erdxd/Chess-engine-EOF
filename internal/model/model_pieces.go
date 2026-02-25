e2df860f7a5bfea0dd6e3563844116b9f9b48ea7
erdxd <tytalexsey@mail.ru>
2026-02-25 12:08:54 +0700

    add logic for bishop

--- internal/model/model_pieces.go
+++ internal/model/model_pieces.go
@@ -229,6 +229,55 @@ func MoveMaker(b *Board, mFX, mFY, mHX, mHY int, pieces int) (*Board, bool) {
 			}
 
 		}
+	case pieces == Bishop || pieces == BishopE:
+		if math.Abs(float64(mHX)-float64(mFX)) != math.Abs(float64(mHY)-float64(mFY)) {
+			return b, false
+		}
+		if b.White {
+			if b.Board[mHX][mHY] == 99 || b.Board[mHX][mHY] > 0 {
+				CanMove = false
+				MoveWhite = true
+			} else {
+				var stepY int
+				var stepX int
+
+				ClearPath := true
+				// вверх налево
+				if mHY > mFY && mFX > mHX {
+					stepY = 1
+					stepX = -1
+
+				} else if mHY > mFY && mHX > mFX { // вверх направо
+					stepX = 1
+					stepY = 1
+				} else if mHY < mFY && mHX < mFX { // вниз влево
+					stepX = -1
+					stepY = -1
+				} else if mFY > mHY && mFX < mHX { //вниз направо
+					stepY = -1
+					stepX = 1
+				}
+				currX := mFX + stepX
+				currY := mFY + stepY
+				for currX != mHX || currY != mHY {
+					if b.Board[currX][currY] != 0 {
+						ClearPath = false
+						break
+					}
+					currX += stepX
+					currY += stepY
+				}
+				if b.Board[mHX][mHY] < 0 && ClearPath == true {
+
+					CanMove = true
+					MoveWhite = true
+				} else if ClearPath == true {
+					CanMove = true
+					MoveWhite = true
+				}
+
+			}
+		}
 	}
 
 	if CanMove {
